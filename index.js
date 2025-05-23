2283f07839027c08862f77e0c763b6fcba67dfc5
seth1nk <kostik.vs10@mail.ru>
2025-05-23 19:51:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://dapper-ganache-144f76.netlify.app/',
     credentials: true,
 }));
 app.use('/css', express.static(path.join(__dirname, 'css')));
